9d295b8ff8bc3c96891c67254370f59bdbd24e2d
Jouni Alanen <jouni@diory.me>
2024-09-25 19:52:02 +0300

    Initial commit for mobile-proto

---
+++ README.md
@@ -0,0 +1,28 @@
+# Mobile-proto
+
+Diosphere proto app for mobile and online usage experimenting
+
+## Run
+
+```
+yarn
+yarn dev
+```
+
+## Config.json
+
+Replace the placeholders in config.json (TODO: Read from .env or .gitignore config.json)
+
+```
+{
+  "region": "YOUR_AWS_REGION",
+  "userPoolId": "YOUR_COGNITO_USER_POOL_ID",
+  "clientId": "YOUR_COGNITO_APP_CLIENT_ID"
+}
+```
+
+## Features
+
+1. Sign-up
+2. Login
+3. List dummy list with "Make API request" button
---
+++ src/homePage.tsx
@@ -0,0 +1,46 @@
+// Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+// SPDX-License-Identifier: Apache-2.0
+
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+
+const HomePage = () => {
+  const [dummyList, setDummyList] = useState([]);
+
+  const navigate = useNavigate();
+
+  const handleApiRequest = async () => {
+    try {
+      const response = await fetch("https://dummyjson.com/todos", {
+        // credentials: "include",
+      });
+      setDummyList((await response.json()).todos);
+    } catch (error) {
+      console.error("API request failed:", error);
+    }
+  };
+
+  const handleLogout = () => {
+    sessionStorage.clear();
+    navigate("/login");
+  };
+
+  return (
+    <div>
+      <h1>Hello World</h1>
+      <button onClick={handleApiRequest}>Make API request</button>
+      <button onClick={handleLogout}>Logout</button>
+      {dummyList.map(({ todo }, index) => (
+        <div
+          key={index}
+          style={{ cursor: "pointer" }}
+          onClick={() => alert(`You clicked dummy: ${todo}`)}
+        >
+          {todo}
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default HomePage;
