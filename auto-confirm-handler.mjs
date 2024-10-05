90d9f76a4a7ab997624fac6f11a61a207a4e82b3
Jouni Alanen <jouni@diory.me>
2024-10-05 16:16:11 +0300

    Auto-confirm emails with specific pattern
    - for robot tests

---
+++ auto-confirm-handler.mjs
@@ -0,0 +1,18 @@
+// Code executed in lambda when users sign up
+// - Pre sign-up Lambda trigger
+
+export const handler = async (event) => {
+  // Extract the email from the user sign-up request
+  const email = event.request.userAttributes.email;
+
+  // If the email matches the pattern "test-*******@example.com", auto-confirm the user
+  const testEmailPattern = /^*****************$/;
+
+  if (testEmailPattern.test(email)) {
+    event.response.autoConfirmUser = true; // Automatically confirm the user
+    event.response.autoVerifyEmail = true; // Automatically verify the user's email
+  }
+
+  // Return the modified event
+  return event;
+};
