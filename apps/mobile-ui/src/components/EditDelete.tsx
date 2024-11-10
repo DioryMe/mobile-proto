e61de182314eed7d8f139c84e3945db0b1249376
Jouni Alanen <jouni@diory.me>
2024-11-10 21:27:49 +0200

    NavBar component

--- apps/mobile-ui/src/components/EditDelete.tsx
+++ apps/mobile-ui/src/components/EditDelete.tsx
@@ -1,11 +1,11 @@
 import { useNavigate } from "react-router-dom";
-
+import NavBar from "./NavBar";
 const EditDelete = () => {
   const navigate = useNavigate();
 
   return (
     <div>
-      <button onClick={() => navigate("/")}>Back to Home</button>
+      <NavBar />
       <h2>Edit & Delete</h2>
       {/* Add your EditDelete content here */}
     </div>
--- apps/mobile-ui/src/components/Search.tsx
+++ apps/mobile-ui/src/components/Search.tsx
@@ -1,11 +1,12 @@
 import { useNavigate } from "react-router-dom";
+import NavBar from "./NavBar";
 
 const Search = () => {
   const navigate = useNavigate();
 
   return (
     <div>
-      <button onClick={() => navigate("/")}>Back to Home</button>
+      <NavBar />
       <h2>Search</h2>
       {/* Add your Search content here */}
     </div>
