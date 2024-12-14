6a931414bfcf03dc29c3233a6013c9e6e0f0924c
Jouni Alanen <jouni@diory.me>
2024-12-14 20:25:02 +0200

    Fix build: jwkToPem.default -> jwkToPem

--- apps/mobile-server/src/auth/auth.service.ts
+++ apps/mobile-server/src/auth/auth.service.ts
@@ -43,7 +43,7 @@ export class AuthService {
       throw new Error('Invalid public key');
     }
 
-    return jwt.verify(token, jwkToPem.default(publicKey), {
+    return jwt.verify(token, jwkToPem(publicKey), {
       algorithms: ['RS256'],
     });
   }
