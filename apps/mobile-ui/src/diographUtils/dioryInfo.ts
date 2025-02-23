2b18f26251a6de1ce20d68c3a7e9e3ca181d9d75
Jouni Alanen <jouni@diory.me>
2025-02-23 19:55:11 +0200

    Initial interface for DioryInfo
    - getDioryInfo returns placeholder values in correct structure

--- apps/mobile-ui/src/diographUtils/dioryInfo.ts
+++ apps/mobile-ui/src/diographUtils/dioryInfo.ts
@@ -3,10 +3,50 @@ import { IDiographObject } from "@diograph/diograph/types";
 
 export interface DioryInfo {
   diograph: Diograph | null;
+  focusId: string;
+  storyId: string;
+  prev: string;
+  next: string;
+  focus: {
+    content: { contentUrl: string };
+    image: string;
+    geo: string;
+    date: string;
+    linked: string[];
+  };
+  relatedGeo: string[];
+  relatedTime: string[];
+  relatedStories: string[];
+  delete?: () => void;
+  link?: () => void;
+  edit?: () => void;
 }
 
 export const getDioryInfo = (diographJson: IDiographObject): DioryInfo => {
   return {
     diograph: new Diograph(diographJson),
+    focusId: "focusId",
+    storyId: "storyId",
+    prev: "prev",
+    next: "next",
+    focus: {
+      content: { contentUrl: "content" },
+      image: "image",
+      geo: "geo",
+      date: "date",
+      linked: ["linked"],
+    },
+    relatedGeo: ["relatedGeo"],
+    relatedTime: ["relatedTime"],
+    relatedStories: ["relatedStories"],
+    delete: () => {
+      return;
+    },
+    link: () => {
+      return;
+    },
+    edit: () => {
+      return;
+    },
   };
 };
