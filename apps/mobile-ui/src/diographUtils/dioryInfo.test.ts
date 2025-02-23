de3495d86d3b83b620ff237f37113f36c064af2b
Jouni Alanen <jouni@diory.me>
2025-02-23 21:41:29 +0200

    Authentic test for default diograph with only root diory

--- apps/mobile-ui/src/diographUtils/dioryInfo.test.ts
+++ apps/mobile-ui/src/diographUtils/dioryInfo.test.ts
@@ -4,22 +4,35 @@ import { getDioryInfo, DioryInfo } from "./dioryInfo";
 jest.mock("uuid", () => ({
   v4: jest.fn(() => "8670abb3-253a-4750-b5a7-1476d2effd60"),
 }));
-describe("getDioryInfo", () => {
-  const mockDiographJson: IDiographObject = {
-    "/": {
-      id: "/",
-      text: "Mocked root diory",
-      created: new Date().toISOString(),
-      modified: new Date().toISOString(),
-    },
-  };
 
-  it("should return a DioryInfo object", () => {
+const mockDiographJson: IDiographObject = {
+  "/": {
+    id: "/",
+    text: "Mocked root diory",
+    created: new Date().toISOString(),
+    modified: new Date().toISOString(),
+  },
+};
+
+describe("getDioryInfo", () => {
+  it("returns a DioryInfo object based on mockDiographJson", () => {
     const dioryInfo: DioryInfo = getDioryInfo(mockDiographJson);
-    expect(dioryInfo).toBeDefined();
-    expect(dioryInfo.diograph?.toObject()["/"].text).toEqual(
-      "Mocked root diory"
-    );
-    expect(dioryInfo.focusId).toBe("focusId");
+
+    expect(dioryInfo).toMatchObject({
+      diograph: expect.any(Object),
+      storyId: null,
+      stories: [],
+      focusId: "/",
+      prev: null,
+      next: null,
+      focus: {
+        content: null, // { contentUrl: "content" },
+        image: null,
+        latlng: null,
+        date: null,
+        linked: [],
+      },
+      focusDiory: expect.any(Object),
+    });
   });
 });
