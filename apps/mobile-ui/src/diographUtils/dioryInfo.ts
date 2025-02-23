0603ed865d0a94c1bcb6862f202d5115026438f3
Jouni Alanen <jouni@diory.me>
2025-02-23 21:41:29 +0200

    Authentic test for default diograph with only root diory

--- apps/mobile-ui/src/diographUtils/dioryInfo.ts
+++ apps/mobile-ui/src/diographUtils/dioryInfo.ts
@@ -1,41 +1,52 @@
 import { Diograph } from "@diograph/diograph";
-import { IDiographObject } from "@diograph/diograph/types";
+import { IDiographObject, IDiory } from "@diograph/diograph/types";
 
 export interface DioryInfo {
   diograph: Diograph | null;
   focusId: string;
-  storyId: string;
-  prev: string;
-  next: string;
+  storyId: string | null;
+  stories: string[];
+  prev: string | null;
+  next: string | null;
   focus: {
-    content: { contentUrl: string };
-    image: string;
-    geo: string;
-    date: string;
+    content: { contentUrl: string } | null;
+    image: string | null;
+    latlng: string | null;
+    date: string | null;
     linked: string[];
   };
+  focusDiory: IDiory;
   relatedGeo: string[];
   relatedTime: string[];
   relatedStories: string[];
-  delete?: () => void;
-  link?: () => void;
-  edit?: () => void;
+  delete: () => void;
+  link: () => void;
+  edit: () => void;
 }
 
-export const getDioryInfo = (diographJson: IDiographObject): DioryInfo => {
+export const getDioryInfo = (
+  diographJson: IDiographObject,
+  focusId: string = "/",
+  storyId?: string
+): DioryInfo => {
+  const diograph = new Diograph(diographJson);
+  const focusDiory = diograph.getDiory({ id: focusId });
+  const { image, latlng, date } = focusDiory;
   return {
     diograph: new Diograph(diographJson),
-    focusId: "focusId",
-    storyId: "storyId",
-    prev: "prev",
-    next: "next",
+    focusId: focusDiory.id,
+    storyId: null,
+    stories: [],
+    prev: null,
+    next: null,
     focus: {
-      content: { contentUrl: "content" },
-      image: "image",
-      geo: "geo",
-      date: "date",
-      linked: ["linked"],
+      content: null, // { contentUrl: "content" },
+      image: image || null,
+      latlng: latlng || null,
+      date: date || null,
+      linked: [],
     },
+    focusDiory,
     relatedGeo: ["relatedGeo"],
     relatedTime: ["relatedTime"],
     relatedStories: ["relatedStories"],
