ef2a9471e57e24db3d96e7eec2f42688e5235de5
Jouni Alanen <jouni@diory.me>
2025-03-01 16:23:11 +0200

    Don't send frontend errors to sentry from localhost

--- apps/mobile-ui/src/main.tsx
+++ apps/mobile-ui/src/main.tsx
@@ -16,7 +16,7 @@ Sentry.init({
   // Tracing
   tracesSampleRate: 1.0, //  Capture 100% of the transactions
   // Set 'tracePropagationTargets' to control for which URLs distributed tracing should be enabled
-  tracePropagationTargets: ["localhost", import.meta.env.VITE_BASE_URL],
+  tracePropagationTargets: [import.meta.env.VITE_BASE_URL],
   // Session Replay
   replaysSessionSampleRate: 0.0, // This sets the sample rate at 10%. You may want to change it to 100% while in development and then sample at a lower rate in production.
   replaysOnErrorSampleRate: 1.0, // If you're not already sampling the entire session, change the sample rate to 100% when sampling sessions where errors occur.
